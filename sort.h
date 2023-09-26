4b60dc3dc83584e540f040062c5ba824cbde0c5f
Jyde24 <chukwunonsojide.e@gmail.com>
2023-09-26 04:51:14 +0000

    Update header file

--- sort.h
+++ sort.h
@@ -27,4 +27,10 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 
+/* swap functions */
+void int_swap(int *a, int *b);
+void node_swap(listint_t **h, listint_t **n1, listint_t *n2);
+void swap_value(int *a, int *b);
+void bubble_sort(int *array, size_t size);
+
 #endif
